896e452fdd662cf277a31fb6b1283d7349dc1dfa
Demetrios <jkopatsis@gmail.com>
2023-04-09 16:02:02 -0700

    state-based resume rendering

--- cv-application/src/App.tsx
+++ cv-application/src/App.tsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
 import Education from "./components/Education";
 import Info from "./components/Info";
 import Jobs from "./components/Jobs";
 import Resume from "./components/Resume";
 
 function App() {
+  const starterInfo = {
+    firstname: "FirstName",
+    lastname: "LastName",
+    email: "email@email.com",
+    phone: "111-222-3333",
+    linkedin: "",
+    site: "",
+  };
+
+  const starterJob = [
+    {
+      jobname: "Job Title",
+      compname: "Company Name",
+      start: "00/00/000",
+      end: "99/99/9999",
+      desc: "Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah. Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah. Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah.",
+    },
+  ];
+
+  const starterEdu = [
+    {
+      degname: "Degree Title",
+      schoolname: "School Name",
+      start: "00/00/000",
+      end: "99/99/9999",
+      desc: "Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah. Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah. Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah.",
+    },
+  ];
+
+  const [infoData, setInfoData] = useState(starterInfo);
+  const [jobData, setJobData] = useState(starterJob);
+  const [eduData, setEduData] = useState(starterEdu);
+
   return (
     <div className="whole">
       <div className="left">
@@ -17,7 +51,11 @@ function App() {
       </div>
       <div>
         <h1>Resume:</h1>
-        <Resume></Resume>
+        <Resume
+          infoData={infoData}
+          jobData={jobData}
+          eduData={eduData}
+        ></Resume>
       </div>
     </div>
   );
--- cv-application/src/components/Resume.tsx
+++ cv-application/src/components/Resume.tsx
@@ -1,58 +1,60 @@
 import React from "react";
 
-const Resume = () => {
+interface Props {
+  infoData: any;
+  jobData: any[];
+  eduData: any[];
+}
+
+const Resume = ({ infoData, jobData, eduData }: Props) => {
   return (
     <div className="resume">
       <div className="Inform">
-        <h2>LastName, FirstName</h2>
+        <h2>
+          {infoData.lastname}, {infoData.firstname}
+        </h2>
         <div className="phonemail oneline">
-          <div>email@email.com</div>
-          <div>111-222-3333</div>
+          <div>{infoData.email}</div>
+          <div>{infoData.phone}</div>
         </div>
         <div className="optionals oneline">
-          <div></div>
-          <div></div>
+          <div>{infoData.linkedin.length >= 0 && infoData.linkedin}</div>
+          <div>{infoData.site.length >= 0 && infoData.site}</div>
         </div>
       </div>
       <div className="Work">
         <h3>Work Experience</h3>
-        <div className="work-unit">
-          <div className="title-company oneline">
-            <div>Job Title</div>
-            <div>Company Name</div>
+        {jobData.map((item, index) => (
+          <div className="work-unit">
+            <div className="title-company oneline">
+              <div>{item.jobname}</div>
+              <div>{item.compname}</div>
+            </div>
+            <div className="jdates oneline">
+              <div>Start Date: {item.start}</div>
+              <div>End Date: {item.end}</div>
+            </div>
+            <div>Description of Duties: </div>
+            <p>{item.desc}</p>
           </div>
-          <div className="jdates oneline">
-            <div>Start Date: 00/00/0000</div>
-            <div>End Date: 99/99/9999</div>
-          </div>
-          <div>Description of Duties: </div>
-          <p>
-            Blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah. Blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah. Blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah.
-          </p>
-        </div>
+        ))}
       </div>
       <div className="Edu">
         <h3>Academic Information</h3>
-        <div className="edu-unit">
-          <div className="title-school oneline">
-            <div>Degree Title</div>
-            <div>School Name</div>
+        {eduData.map((item, index) => (
+          <div className="edu-unit">
+            <div className="title-school oneline">
+              <div>{item.degname}</div>
+              <div>{item.schoolname}</div>
+            </div>
+            <div className="edates oneline">
+              <div>Start Date: {item.start}</div>
+              <div>End Date: {item.end}</div>
+            </div>
+            <div>Description of Degree: </div>
+            <p>{item.desc}</p>
           </div>
-          <div className="edates oneline">
-            <div>Start Date: 00/00/0000</div>
-            <div>End Date: 99/99/9999</div>
-          </div>
-          <div>Description of Degree: </div>
-          <p>
-            Blah blah blah blah blah blah blah blah blah blah blah blah blah
-            blah blah blah blah. Blah blah blah blah blah blah blah blah blah
-            blah blah blah blah blah blah blah blah. Blah blah blah blah blah
-            blah blah blah blah blah blah blah blah blah blah blah blah.
-          </p>
-        </div>
+        ))}
       </div>
     </div>
   );
