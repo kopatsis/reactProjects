66d6195a9a8b4b9ee5a552562bfa81c720a24a28
Demetrios <jkopatsis@gmail.com>
2023-04-09 14:55:56 -0700

    entry setup present

--- cv-application/src/App.tsx
+++ cv-application/src/App.tsx
@@ -1,9 +1,22 @@
+import Education from "./components/Education";
 import Info from "./components/Info";
+import Jobs from "./components/Jobs";
 
 function App() {
   return (
-    <div>
-      <Info></Info>
+    <div className="whole">
+      <div>
+        <h1>Enter/Update Info:</h1>
+        <h2>General Information</h2>
+        <Info></Info>
+        <h2>Work Experience</h2>
+        <Jobs></Jobs>
+        <h2>Academic Information</h2>
+        <Education></Education>
+      </div>
+      <div>
+        <h1>Resume:</h1>
+      </div>
     </div>
   );
 }
--- cv-application/src/components/Jobs.tsx
+++ cv-application/src/components/Jobs.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Jobs = () => {
-  return <div>Jobs</div>;
+  return (
+    <form>
+      <input id="title" placeholder="Job Title" type="text" required />
+      <input id="company" placeholder="Company Name" type="text" required />
+      <div className="date-range">
+        <input id="start-job" placeholder="Start" type="date" required />
+        <input id="end-job" placeholder="End" type="date" required />
+      </div>
+      <textarea rows={4} id="desc" placeholder="Role Description"></textarea>
+      <input id="sub" type="submit" value="Add New" />
+    </form>
+  );
 };
 
 export default Jobs;
