70fc6b603c95567f820d565bafe36b2fad6fefd5
Demetrios <jkopatsis@gmail.com>
2023-04-12 14:31:37 -0700

    instructions/titles presented

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -1,38 +1,39 @@
 import React from 'react'
+import Ancillary from './components/Ancillary';
 
 const App = () => {
 
   const nameMap = {
-    "Edward Elric": "./assets/edward.jpg",
-    "Eren Yeager": "./assets/eren.jpg",
-    "Gintoki Sakata": "./assets/gintoki.jpg",
-    "Satorou Gojo": "./assets/gojo.jpg",
-    "Guts": "./assets/guts.jpg",
-    "Hachiman Hikigaya": "./assets/hachiman.jpg",
-    "Itachi Uchiha": "./assets/itachi.jpg",
-    "Ken Kaneki": "./assets/kaneki.jpg",
-    "Killua Zoldyck": "./assets/killua.jpg",
-    "L Lawliet": "./assets/L.jpg",
-    "Lelouch Lamperouge": "./assets/lelouch.jpg",
-    "Levi Ackerman": "./assets/levi.jpg",
-    "Light Yagami": "./assets/light.jpg",
-    "Monkey D Luffy": "./assets/luffy.jpg",
-    "Makise Kurisu": "./assets/makise.jpg",
-    "Mikasa Ackerman": "./assets/mikasa.jpg",
-    "Naruto Uzumaki": "./assets/naruto.jpg",
-    "Rintarou Okabe": "./assets/rintaro.jpg",
-    "Saitama": "./assets/saitama.jpg",
-    "Roronoa Zoro": "./assets/zoro.jpg",
+    "Edward Elric": "../assets/edward.jpg",
+    "Eren Yeager": "../assets/eren.jpg",
+    "Gintoki Sakata": "../assets/gintoki.jpg",
+    "Satorou Gojo": "../assets/gojo.jpg",
+    "Guts": "../assets/guts.jpg",
+    "Hachiman Hikigaya": "../assets/hachiman.jpg",
+    "Itachi Uchiha": "../assets/itachi.jpg",
+    "Ken Kaneki": "../assets/kaneki.jpg",
+    "Killua Zoldyck": "../assets/killua.jpg",
+    "L Lawliet": "../assets/L.jpg",
+    "Lelouch Lamperouge": "../assets/lelouch.jpg",
+    "Levi Ackerman": "../assets/levi.jpg",
+    "Light Yagami": "../assets/light.jpg",
+    "Monkey D Luffy": "../assets/luffy.jpg",
+    "Makise Kurisu": "../assets/makise.jpg",
+    "Mikasa Ackerman": "../assets/mikasa.jpg",
+    "Naruto Uzumaki": "../assets/naruto.jpg",
+    "Rintarou Okabe": "../assets/rintaro.jpg",
+    "Saitama": "../assets/saitama.jpg",
+    "Roronoa Zoro": "../assets/zoro.jpg",
   };
 
   const usedSet = [];
 
-  const unUsedSet = [];
-
-  nameMap.forEach(name => unUsedSet.push(name));
+  const unUsedSet = Object.keys(nameMap);
 
   return (
-    <div>App</div>
+    <div>
+      <Ancillary/>
+    </div>
   )
 }
 
--- memory-card/src/components/Ancillary.jsx
+++ memory-card/src/components/Ancillary.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Ancillary = () => {
   return (
-    <div>Ancillary</div>
+    <>
+        <div className='topLine'>
+            <h1>Anime Character Memory Game</h1>
+            <div className='metrics'>
+                <div>Score: 0</div>
+                <div>Best: 0</div>
+            </div>
+        </div>
+        <div className='bottomLine'>
+            <p>To play this game, click any character from the below options to select them. Once they are clicked, a new round will be started and new characters will be shown. 
+                For each round, choose a character that you have not selected previously to increase your score. Try to get all 20 characters without choosing one that you have already clicked. 
+                Good luck! 
+            </p>
+        </div>
+    </>
   )
 }
 
