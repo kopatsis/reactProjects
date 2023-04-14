495c3b38427dfc8145281d93c23ed1fbd9646580
Demetrios <jkopatsis@gmail.com>
2023-04-13 21:28:13 -0700

    more mobile friendly ability

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -19,6 +19,12 @@ const App = () => {
     <div>
       <Ancillary passScore={Score}/>
       <Cardset scoreZero={zeroScore} scoreInc={upScore} score={Score}/>
+      <div className='bottomLine'>
+            <p>To play this game, click any character from the below options to select them. Once they are clicked, a new round will be started and new characters will be shown. 
+                For each round, choose a character that you have not selected previously to increase your score. Try to get all 20 characters without choosing one that you have already clicked. 
+                Good luck! 
+            </p>
+        </div>
     </div>
   )
 }
--- memory-card/src/components/Ancillary.jsx
+++ memory-card/src/components/Ancillary.jsx
@@ -19,12 +19,6 @@ const Ancillary = ({passScore}) => {
                 <div>Best: {best}</div>
             </div>
         </div>
-        <div className='bottomLine'>
-            <p>To play this game, click any character from the below options to select them. Once they are clicked, a new round will be started and new characters will be shown. 
-                For each round, choose a character that you have not selected previously to increase your score. Try to get all 20 characters without choosing one that you have already clicked. 
-                Good luck! 
-            </p>
-        </div>
     </>
   )
 }
