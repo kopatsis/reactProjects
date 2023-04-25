99d75b7b934661bbe5b5bf34e36f432b36be5257
Demetrios <jkopatsis@gmail.com>
2023-04-24 20:28:09 -0700

    scoreboard metrics

--- BAAS/src/App.tsx
+++ BAAS/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Game from "./components/Game";
@@ -9,11 +9,20 @@ import FindMessage from "./components/FindMessage";
 import Winner from "./components/Winner";
 
 const App = () => {
+  const [scoreboard, setScoreboard] = useState([
+    { name: "Example Reemachine", time: [0, 180, 22], seconds: 202 },
+    { name: "Other Person", time: [0, 120, 14], seconds: 134 },
+    { name: "ugggg idk", time: [0, 240, 1], seconds: 241 },
+  ]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/home" element={<Home />}>
-        <Route path="score" element={<Scoreboard />} />
+        <Route
+          path="score"
+          element={<Scoreboard board={scoreboard} special={0} />}
+        />
       </Route>
       <Route path="/game" element={<Game />}>
         <Route path="exit" element={<ExitMessage />} />
--- BAAS/src/components/Scoreboard.tsx
+++ BAAS/src/components/Scoreboard.tsx
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Scoreboard = () => {
+interface Props {
+  board: any;
+  special: any;
+}
+
+const Scoreboard = ({ board, special }: Props) => {
   const navigate = useNavigate();
+
+  const [sortedBoard, setSortedBoard] = useState([]);
+
+  useEffect(() => {
+    let temp = board;
+    temp.sort((a: any, b: any) => {
+      return a.seconds - b.seconds;
+    });
+    setSortedBoard(temp);
+  }, [board]);
 
   return (
     <div className="popup-outer">
       <div className="popup-inner">
         <button onClick={() => navigate("/home")}>X Close</button>
         <div>Scoreboard</div>
+        <div className="allscores">
+          {special !== 0 && (
+            <div className="score-entry">
+              <div>Your entry:</div>
+              <div>{special.name}</div>
+              <div>
+                {special.time[0] / 3600}h {special.time[1] / 60}m{" "}
+                {special.time[2]}s
+              </div>
+            </div>
+          )}
+          <div className="score-entry">
+            <div>Place: </div>
+            <div>Name: </div>
+            <div>Time: </div>
+          </div>
+          {sortedBoard.map((item: any, index: number) => (
+            <div className="score-entry" key={index}>
+              <div>{index + 1}</div>
+              <div>{item.name}</div>
+              <div>
+                {item.time[0] / 3600}h {item.time[1] / 60}m {item.time[2]}s
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
